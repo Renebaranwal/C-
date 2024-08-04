1e84e3ed24e273c6dfc01a5cc6c66dbdb99f0d08
Rene baranwal <121424157+Renebaranwal@users.noreply.github.com>
2024-08-05 00:03:49 +0530

    Add files via upload

---
+++ JAVA/SumAvg2DArray.java
@@ -0,0 +1,40 @@
+public class SumAvg2DArray {
+    public static void main(String[] args) {
+        int[][] numArr = ArrayUtility2D.input2DArray();
+        long sum = sum(numArr);
+        double avg = avg(numArr);
+        System.out.println(sum);
+        System.out.println(avg);
+
+
+    }
+    public static long sum(int[][] numArr)
+    {
+        long sum = 0;
+        int i = 0;
+        while(i < numArr.length)
+        {
+            int j = 0;
+            while(j < numArr[i].length)
+            {
+                sum = sum + numArr[i][j];
+                j++;
+            }
+            i++;
+        }
+        return sum;
+    }
+    public static double avg(int[][] numArr)
+    {
+        if (numArr.length == 0)
+        {
+            return 0;
+        }
+        int rows = numArr.length;
+        int cols = numArr[0].length;
+        double size = rows * cols;
+        return sum(numArr) / size;
+
+    }
+    
+}
