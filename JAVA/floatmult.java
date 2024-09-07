10eba1efd01d08c7c0836d4cf9416597e5dc2cab
Rene baranwal <121424157+Renebaranwal@users.noreply.github.com>
2024-09-07 23:15:02 +0530

    Add files via upload

---
+++ JAVA/floatmult.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class floatmult {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first decimal number");
+        double first = sc.nextDouble();
+        System.out.println("Enter second decimal number: ");
+        double second = sc.nextDouble();
+        double mul = first * second;
+        System.out.println("\n Result is: " + mul);
+
+
+
+
+
+    }
+}
