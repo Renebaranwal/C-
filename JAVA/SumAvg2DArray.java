a3d9f99cf7d26e9a7e53bf3d9021bb5556e94a3b
Rene baranwal <121424157+Renebaranwal@users.noreply.github.com>
2024-09-17 23:07:38 +0530

    Add files via upload

--- JAVA/SumAvg2DArray.java
+++ JAVA/SumAvg2DArray.java
@@ -2,39 +2,36 @@ public class SumAvg2DArray {
     public static void main(String[] args) {
         int[][] numArr = ArrayUtility2D.input2DArray();
         long sum = sum(numArr);
-        double avg = avg(numArr);
+        double avg = average(numArr);
         System.out.println(sum);
         System.out.println(avg);
-
-
-    }
-    public static long sum(int[][] numArr)
+        
+}
+public static long sum(int[][] numArr)
+{
+    long sum = 0;
+    int i = 0;
+    while(i < numArr.length)
     {
-        long sum = 0;
-        int i = 0;
-        while(i < numArr.length)
+        int j = 0;
+        while(j < numArr[i].length)
         {
-            int j = 0;
-            while(j < numArr[i].length)
-            {
-                sum = sum + numArr[i][j];
-                j++;
-            }
-            i++;
+          sum = sum + numArr[i][j]; 
+          j++; 
         }
-        return sum;
+        i++;
     }
-    public static double avg(int[][] numArr)
+    return 0;
+}
+public static double average(int[][] numArr)
+{
+    if (numArr.length == 0)
     {
-        if (numArr.length == 0)
-        {
-            return 0;
-        }
-        int rows = numArr.length;
-        int cols = numArr[0].length;
-        double size = rows * cols;
-        return sum(numArr) / size;
-
+        return 0; 
     }
+    int rows = numArr.length;
+    int cols = numArr[0].length;
+    double size = rows 
+    double
     
 }
