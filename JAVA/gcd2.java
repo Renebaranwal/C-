4a5c0f4dbbf4644b4c070ec92cc1b91b63d34962
Rene baranwal <121424157+Renebaranwal@users.noreply.github.com>
2024-11-29 20:02:21 +0530

    Add files via upload

---
+++ JAVA/gcd2.java
@@ -0,0 +1,23 @@
+public class gcd2 {
+    public static void main(String[] args) {
+        int n1 = 20 , n2 = 15;
+        int gcd = finalGcd(n1 , n2);
+        System.out.println("GCD of " + n1 + " and " + n2 + "is: " + gcd);
+    }
+
+public static int finalGcd(int n1 , int n2)
+{
+    int gcd = 1;
+    for(int i = 1; i <= Math.min(n1 , n2); i++)
+    {
+    
+        if (n1 % i == 0 && n2 % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+}
+
+    
