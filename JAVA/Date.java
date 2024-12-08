89cd7c8e7286526d578a7798880ef7d849867981
Rene baranwal <121424157+Renebaranwal@users.noreply.github.com>
2024-12-08 23:26:48 +0530

    Add files via upload

---
+++ JAVA/Date.java
@@ -0,0 +1,15 @@
+class Date {
+    Date(int year,int month,int day,int hrs,int min)
+    {
+        System.out.println("year: " + year + "month: " + month + "day: " + day + "hrs: " + hrs + "min: " + min);
+    }}
+    class Main
+    {
+        public static void main(String args[])
+        {
+            Date ob=new Date(2004,4,12,2,41);
+        }
+    }
+    
+    
+  
