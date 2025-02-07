c2f5c9ef6562d79c4631c3f16556f1e32f8fc2b6
Rene baranwal <121424157+Renebaranwal@users.noreply.github.com>
2025-02-07 22:56:46 +0530

    Add files via upload

---
+++ JAVA/Wrapper.java
@@ -0,0 +1,11 @@
+public class Wrapper {
+    public static void main(String args[])
+    {
+        String a =args[0]
+        float b = Float.parseFloat(args[1]);
+        System.out.println(a));
+        System.out.println(b);
+
+    }
+
+}
